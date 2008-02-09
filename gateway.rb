4e6ca0c354130942f15fa0c7c9e3ae5e37d127f7
Garry Dolley <gdolley@arpnetworks.com>
2008-02-09 03:43:38 -0800

    Support for live and test environments in gateway.rb

--- gateway.rb
+++ gateway.rb
@@ -1,10 +1,19 @@
 config = YAML.load(File.read(File.join(File.dirname(__FILE__), 'gateway.yml')))
 
-$API_LOGIN        = config['api_login']
-$API_PASSWORD     = config['api_password']
-$API_KEY_FILENAME = config['api_key_filename']
+mode = config['live']['enabled'] ? 'live' : 'test'
+
+$API_LOGIN        = config[mode]['api_login']
+$API_PASSWORD     = config[mode]['api_password']
+$API_KEY_FILENAME = config[mode]['api_key_filename']
+
+if mode == 'test'
+  ActiveMerchant::Billing::Base.mode = :test
+  ActiveMerchant::Billing::CreditCard.require_verification_value = false
+end
 
 @gateway = ActiveMerchant::Billing::PaypalGateway.new(
-  :login    => $API_LOGIN,
-  :password => $API_PASSWORD,
-  :pem      => File.read(File.join(File.dirname(__FILE__), $API_KEY_FILENAME)))
+  { :login     => $API_LOGIN,
+    :password  => $API_PASSWORD }.merge(
+  mode == 'live' ? 
+  { :pem       => File.read(File.join(File.dirname(__FILE__), $API_KEY_FILENAME)) } :
+  { :signature => config['test']['api_signature'] }))
